b375fb495378ea68d04ce0ba8c6c6c749eeb4393
Viktoriia <heyvika7@gmail.com>
2023-04-05 16:27:34 -0700

    Added arrangement for grocery list - completed and not items

--- app/src/main/java/com/example/mealprep/fill/out/recipe/card/groceries/GroceriesScreen.kt
+++ app/src/main/java/com/example/mealprep/fill/out/recipe/card/groceries/GroceriesScreen.kt
@@ -1,15 +1,9 @@
 package com.example.mealprep.fill.out.recipe.card
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
@@ -17,17 +11,18 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.ExperimentalUnitApi
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.NavHostController
 import com.example.littlelemon.DishRepository
 import com.example.mealprep.BottomNavigationBar
-import com.example.mealprep.ExpandableCard
 import com.example.mealprep.fill.out.recipe.card.mealplanning.MealPlanningViewModel
 import com.example.mealprep.ui.theme.MealPrepColor
 import com.example.mealprep.ui.theme.fontFamilyForBodyB2
@@ -35,25 +30,31 @@ import com.example.mealprep.ui.theme.fontFamilyForBodyB2
 
 @OptIn(ExperimentalUnitApi::class)
 @Composable
-fun GroceriesScreen(navController: NavHostController, viewModel: MealPlanningViewModel,) {
-    Scaffold(
-        topBar = {
-            TopBarForGroceriesScreen()
-        },
-        bottomBar = {
-            BottomNavigationBar(navController = navController)
-        },
-        content = { padding ->
-            Box(modifier = Modifier.padding(top = 30.dp, start = 16.dp, end = 16.dp, bottom = 60.dp)) {
-                val dish = requireNotNull(DishRepository.getDish(1))
+fun GroceriesScreen(navController: NavHostController, viewModel: MealPlanningViewModel) {
+    Scaffold(topBar = {
+        TopBarForGroceriesScreen()
+    }, bottomBar = {
+        BottomNavigationBar(navController = navController)
+    }, content = { padding ->
+        Column(
+            modifier = Modifier.padding(
+                    top = 30.dp, start = 16.dp, end = 16.dp, bottom = 60.dp
+                ), verticalArrangement = Arrangement.Top
+        ) {
+            val listGroceries = viewModel.listGroceries.observeAsState().value
 
-                val listIngredients = dish.ingredientsList
 
+
+
+            Column(
+                Modifier.wrapContentHeight()
+            ) {
+                var expand by remember { mutableStateOf(false) }
                 val chosenGroceries = viewModel.chosenGroceries.observeAsState().value
 
-                LazyColumn {
-                    if (!listIngredients.isNullOrEmpty()) {
-                        items(listIngredients) { item ->
+                LazyColumn(Modifier.weight(1f)) {
+                    if (!listGroceries.isNullOrEmpty()) {
+                        items(listGroceries) { item ->
                             Column(
                                 modifier = Modifier
                                     .background(Color.White)
@@ -61,28 +62,75 @@ fun GroceriesScreen(navController: NavHostController, viewModel: MealPlanningVie
                                 verticalArrangement = Arrangement.Center,
                                 horizontalAlignment = Alignment.CenterHorizontally
                             ) {
-                                setUpLines(item, viewModel)
+                                setUpLines(item, viewModel, false)
+                            }
+                            if (listGroceries.last() == item) {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(
+                                            top = 20.dp, start = 8.dp, end = 8.dp, bottom = 8.dp
+                                        ),
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Text(
+                                        text = "Completed",
+                                        color = MealPrepColor.orange,
+                                        fontSize = 20.sp,
+                                        textAlign = TextAlign.Start,
+                                        fontWeight = FontWeight.Normal,
+                                        modifier = Modifier
+                                            .weight(.9f)
+                                            .padding(start = 8.dp)
+                                    )
+                                    IconButton(modifier = Modifier
+//                        .rotate(rotationState)
+                                        .weight(.1f), onClick = {
+                                        expand = !expand
+                                    }) {
+                                        Icon(
+                                            imageVector = Icons.Default.KeyboardArrowDown,
+                                            tint = MealPrepColor.orange, // Icon Color
+                                            contentDescription = "Drop Down Arrow"
+                                        )
+                                    }
+                                }
+                            }
+                        }
+
+
+
+                    }
+                    if (expand) {
+                        if (!chosenGroceries.isNullOrEmpty()) {
+                            items(chosenGroceries) { item ->
+                                Column(
+                                    modifier = Modifier.fillParentMaxWidth(),
+                                    verticalArrangement = Arrangement.Center,
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    setUpLines(item, viewModel, true)
+                                }
                             }
                         }
                     }
                 }
-
-                    ExpandableCard("Completed",viewModel, MealPrepColor.orange)
-
-
-
             }
 
+
         }
-    )
+
+
+    })
 
 }
+
 
 @ExperimentalUnitApi
 @Composable
 fun setUpLines(
-    item: String,
-    viewModel: MealPlanningViewModel
+    item: String, viewModel: MealPlanningViewModel, isCompleted: Boolean
 ) {
 
     val chosenGroceries = viewModel.chosenGroceries.observeAsState().value
@@ -99,14 +147,14 @@ fun setUpLines(
         Row(
             modifier = Modifier
                 .fillMaxHeight()
-                .weight(7f), verticalAlignment = Alignment.CenterVertically
+                .weight(7f),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             RadioButton(
                 selected = chosenGroceries?.contains(item) ?: false,
                 modifier = Modifier.size(16.dp),
                 colors = RadioButtonDefaults.colors(
-                    selectedColor = MealPrepColor.orange,
-                    unselectedColor = MealPrepColor.black
+                    selectedColor = MealPrepColor.orange, unselectedColor = MealPrepColor.black
                 ),
                 onClick = {
                     viewModel.performQueryForGroceries(item)
@@ -115,7 +163,11 @@ fun setUpLines(
             )
             Spacer(modifier = Modifier.width(width = 8.dp))
             Text(
-                text = item, fontFamily = fontFamilyForBodyB2,
+                text = item,
+                fontFamily = fontFamilyForBodyB2,
+                style = if (isCompleted) TextStyle(textDecoration = TextDecoration.LineThrough) else TextStyle(
+                    textDecoration = TextDecoration.None
+                ),
                 fontSize = 16.sp
             )
         }
--- app/src/main/java/com/example/mealprep/fill/out/recipe/card/mealplanning/MealPlanningViewModel.kt
+++ app/src/main/java/com/example/mealprep/fill/out/recipe/card/mealplanning/MealPlanningViewModel.kt
@@ -1,12 +1,12 @@
 package com.example.mealprep.fill.out.recipe.card.mealplanning
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.littlelemon.Dish
-import com.example.mealprep.fill.out.recipe.card.Groceries
+import com.example.littlelemon.DishRepository
 
 class MealPlanningViewModel : ViewModel() {
+
     private var _list = MutableLiveData<List<Dish>?>()
 
     val list: MutableLiveData<List<Dish>?>
@@ -20,24 +20,44 @@ class MealPlanningViewModel : ViewModel() {
         get() = _chosenGroceries
 
 
+    private var _listGroceries = MutableLiveData<List<String>?>()
+
+    //Todo: later make listGroceries - type Groceries
+    val listGroceries: MutableLiveData<List<String>?>
+        get() = _listGroceries
+
+    init {
+        val dish = requireNotNull(DishRepository.getDish(1))
+
+        val listIngredients = dish.ingredientsList
+        _listGroceries.value = listIngredients
+
+    }
     fun performQuery(
         dish: Dish
     ) {
-        if(_list.value?.contains(dish) == true){
+        if (_list.value?.contains(dish) == true) {
             _list.value = _list.value?.minus(dish) ?: listOf(dish)
-        }else{
+        } else {
             _list.value = _list.value?.plus(dish) ?: listOf(dish)
         }
     }
 
+
     fun performQueryForGroceries(
         item: String
     ) {
-        if(_chosenGroceries.value?.contains(item) == true){
+        if (_chosenGroceries.value?.contains(item) == true) {
             _chosenGroceries.value = _chosenGroceries.value?.minus(item) ?: listOf(item)
-        }else{
+            _listGroceries.value = _listGroceries.value?.plus(item) ?: listOf(item)
+        } else {
             _chosenGroceries.value = _chosenGroceries.value?.plus(item) ?: listOf(item)
+            _listGroceries.value = _listGroceries.value?.minus(item) ?: listOf(item)
         }
+    }
+
+    fun addAllGroceries() {
+
     }
 
 
